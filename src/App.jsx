bb6a3711718c46cc7d47fb43ee89f87c9f6fccc8
VerySeriousHireableAccount <phantranh2206@gmail.com>
2023-10-10 14:07:42 -0400

    establish routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import { useState } from "react";
 import Album from "./components/Album"
+import DetailView from "./components/DetailView"
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Toolbar from '@mui/material/Toolbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <Album/>
-      </div>
-    </>
+    <BrowserRouter>
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Pillow
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Routes>
+        <Route path="/*" element={<Album />} />
+        <Route path="/detail" element={<DetailView />} />
+        {/* <Route path="/signin" element={<Signin />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
