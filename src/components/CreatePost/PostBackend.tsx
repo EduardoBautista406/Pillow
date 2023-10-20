937b2b72bd087e264b89ed0b1d6712cc1320d7e2
Elwin He <50185565+elwinhe@users.noreply.github.com>
2023-10-20 00:31:13 -0400

    add listing to database

--- src/components/CreatePost/PostBackend.tsx
+++ src/components/CreatePost/PostBackend.tsx
@@ -2,8 +2,31 @@ import { collection, getDocs, addDoc, serverTimestamp } from 'firebase/firestore
 import { auth, storage, db, uploadString, ref, getDownloadURL } from '../../firebase';
 
 
-export async function addUserDataToDatabase(userData) {
+export async function addUserDataToDatabase(userData, housingData) {
     const listingsCollection = collection(db, 'listings');
+
+    // Convert current date to a string in the format "m/dd/yyyy"
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const year = today.getFullYear();
+    const dateString = `${month}/${day}/${year}`;
+
+    await addDoc(listingsCollection, {
+        address: housingData.address1 + ' ' + housingData.address2,
+        date: serverTimestamp(),
+        description: housingData.review,
+        image: "null",
+        preferences: housingData.gender,
+        price: housingData.price,
+        sqft: housingData.sqft,
+        bedrooms: housingData.beds,
+        bathrooms: housingData.baths,
+        user: userData.email,
+      });    
+}
+
+/*
     try {
         const response = await fetch('https://your-api-endpoint.com/addUser', {
             method: 'POST',
@@ -22,10 +45,4 @@ export async function addUserDataToDatabase(userData) {
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error.message);
     }
-    // Convert current date to a string in the format "m/dd/yyyy"
-    const today = new Date();
-    const day = String(today.getDate()).padStart(2, '0');
-    const month = String(today.getMonth() + 1).padStart(2, '0');
-    const year = today.getFullYear();
-    const dateString = `${month}/${day}/${year}`;
-}
+    */
