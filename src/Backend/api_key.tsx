59d2229e6f82034cc8b70386374e33140ebe74c9
vpolkampally0125 <54823646+vpolkampally0125@users.noreply.github.com>
2023-12-04 23:29:33 -0500

    Update api_key.tsx

--- src/Backend/api_key.tsx
+++ src/Backend/api_key.tsx
@@ -1,2 +1,2 @@
-export const MAPS_API_KEY = "AIzaSyBCQwDmL-auS8BvrMOswmBUp_Xy91WfGaQ"
-export const FIREBASE_API = "AIzaSyBvDDryFaJ2fAqkjEi8Va_UCm9JxOkL5iQ"
+export const MAPS_API_KEY = ""
+export const FIREBASE_API = ""
